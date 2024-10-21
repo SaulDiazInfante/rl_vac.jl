05f286fc5d85b14f6c4a9da4f4863aa23e041b1d
SAUL DIAZ INFANTE VELASCO <sauld@cimat.mx>
2024-10-20 17:21:57 -0700

    testings

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,8 @@
 using rl_vac
 using Test
 using DataFrames, CSV
-
+using Coverage
+import CoverageTools, LibGit2
 p = load_parameters(
     "../data/parameters_model.json"
 )
@@ -84,8 +85,10 @@ x = get_interval_solution!(
 );
 time = x[:, 1]
 #
+
 # TODO: GetSolutionPath documentation
-@testset "jl" begin
+#@code_coverage begin
+@testset "rl_vac.jl" begin
     # Test for load_parameters.jl
     @test(
         load_parameters(
@@ -179,4 +182,120 @@ time = x[:, 1]
     @test(
         nrow(cond) == sum(cond.S)
     )
+
 end
+##
+
+@testset "Coverage" begin
+    # set up base system ENV vars for testing
+    withenv(
+        "DISABLE_AMEND_COVERAGE_FROM_SRC" => nothing,
+        "COVERALLS_TOKEN" => "token_name_1",
+        "COVERALLS_URL" => nothing,
+        "CODECOV_URL" => nothing,
+        "CODECOV_URL_PATH" => nothing,
+        "CODECOV_TOKEN" => nothing,
+        "CODECOV_NAME" => nothing,
+        "CODECOV_FLAGS" => nothing,
+        "TRAVIS" => nothing,
+        "TRAVIS_BRANCH" => nothing,
+        "TRAVIS_COMMIT" => nothing,
+        "TRAVIS_PULL_REQUEST" => nothing,
+        "TRAVIS_BUILD_NUMBER" => nothing,
+        "TRAVIS_JOB_ID" => nothing,
+        "TRAVIS_JOB_NUMBER" => nothing,
+        "TRAVIS_REPO_SLUG" => nothing,
+        "APPVEYOR" => nothing,
+        "APPVEYOR_PULL_REQUEST_NUMBER" => nothing,
+        "APPVEYOR_ACCOUNT_NAME" => nothing,
+        "APPVEYOR_PROJECT_SLUG" => nothing,
+        "APPVEYOR_BUILD_VERSION" => nothing,
+        "APPVEYOR_REPO_BRANCH" => nothing,
+        "APPVEYOR_REPO_COMMIT" => nothing,
+        "APPVEYOR_REPO_NAME" => nothing,
+        "APPVEYOR_BUILD_NUMBER" => nothing,
+        "APPVEYOR_BUILD_ID" => nothing,
+        "APPVEYOR_JOB_ID" => nothing,
+        "GITHUB_ACTION" => nothing,
+        "GITHUB_EVENT_PATH" => nothing,
+        "GITHUB_HEAD_REF" => nothing,
+        "GITHUB_REF" => nothing,
+        "GITHUB_REPOSITORY" => nothing,
+        "GITHUB_RUN_ID" => nothing,
+        "GITHUB_SHA" => nothing,
+        "service_job_id" => nothing,
+        "JENKINS" => nothing,
+        "BUILD_ID" => nothing,
+        "CI_PULL_REQUEST" => nothing,
+        "GIT_BRANCH" => nothing
+    ) do
+
+        @testset "codecovio.jl" begin
+            # these methods are only used for testing the token generation for local repos
+            # and CI whilst not breaking the current API
+            construct_uri_string_local(dir=pwd(); kwargs...) = Coverage.Codecov.construct_uri_string(
+                Coverage.Codecov.add_local_to_kwargs(dir; kwargs...))
+
+            construct_uri_string_ci(; kwargs...) = Coverage.Codecov.construct_uri_string(
+                Coverage.Codecov.add_ci_to_kwargs(; kwargs...))
+
+            # empty file coverage for testing
+            fcs = FileCoverage[]
+
+            # test local submission process (but only if we are in a git repo)
+            _dotgit = joinpath(dirname(@__DIR__), ".git")
+            if isdir(_dotgit) || isfile(_dotgit)
+                LibGit2.with(LibGit2.GitRepoExt(pwd())) do repo
+                    # default values
+                    codecov_url = construct_uri_string_local()
+                    @test occursin("codecov.io", codecov_url)
+                    @test occursin("commit=", codecov_url)
+                    @test occursin("branch=", codecov_url)
+                    @test !occursin("service", codecov_url)
+
+                    # env var url override
+                    withenv("CODECOV_URL" => "https://enterprise-codecov-1.com") do
+
+                        codecov_url = construct_uri_string_local()
+                        @test occursin("enterprise-codecov-1.com", codecov_url)
+                        @test occursin("commit=", codecov_url)
+                        @test occursin("branch=", codecov_url)
+                        @test !occursin("service", codecov_url)
+
+                        # function argument url override
+                        codecov_url = construct_uri_string_local(codecov_url="https://enterprise-codecov-2.com")
+                        @test occursin("enterprise-codecov-2.com", codecov_url)
+                        @test occursin("commit=", codecov_url)
+                        @test occursin("branch=", codecov_url)
+                        @test !occursin("service", codecov_url)
+                        @test !occursin("name", codecov_url)
+                        @test !occursin("flags", codecov_url)
+
+                        # env var token
+                        withenv("CODECOV_TOKEN" => "token_name_1",
+                            "CODECOV_NAME" => "cv_name",
+                            "CODECOV_FLAGS" => "cv_flags") do
+
+                            codecov_url = construct_uri_string_local()
+                            @test occursin("enterprise-codecov-1.com", codecov_url)
+                            @test occursin("token=token_name_1", codecov_url)
+                            @test occursin("name=cv_name", codecov_url)
+                            @test occursin("flags=cv_flags", codecov_url)
+                            @test !occursin("service", codecov_url)
+
+                            # function argument token url override
+                            codecov_url = construct_uri_string_local(token="token_name_2")
+                            @test occursin("enterprise-codecov-1.com", codecov_url)
+                            @test occursin("token=token_name_2", codecov_url)
+                            @test !occursin("service", codecov_url)
+                        end
+                    end
+                end
+            else
+                @warn "skipping local repo tests for Codecov, since not a git repo"
+            end
+        end
+    end
+    #Coverage.report()
+end
+
