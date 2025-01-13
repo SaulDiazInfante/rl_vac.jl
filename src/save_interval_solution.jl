f8ec8be6736a550f10cccabe67e202f64ec40111
SaulDiazInfante <saul.diazinfante@unison.mx>
2025-01-13 12:21:35 -0700

    passing test

--- src/save_interval_solution.jl
+++ src/save_interval_solution.jl
@@ -27,7 +27,8 @@ function save_interval_solution(x;
                         "I_S", "I_A", "R",
                         "D", "V", "CL",
                         "X_vac", "X_0_mayer", "K_stock",
-                        "action", "opt_policy"
+        "action", "opt_policy",
+        "t_index_interval"
                     ],
                 file_name = "solution_interval.csv"
 )
