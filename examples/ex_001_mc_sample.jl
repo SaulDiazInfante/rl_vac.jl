7af4f6922f8bb462731b23760388419f83007bc7
SAUL DIAZ INFANTE VELASCO <sauld@cimat.mx>
2025-01-24 09:57:51 -0700

    local office commit

--- examples/ex_001_mc_sample.jl
+++ examples/ex_001_mc_sample.jl
@@ -3,7 +3,7 @@ using DataFrames, CSV, JSON
 using CairoMakie
 CairoMakie.activate!()
 
-sampling_size = 10000
+sampling_size = 10
 df_par, df_mc, path_par, path_mc = montecarlo_sampling(
     sampling_size,
     "data/parameters_model.json"
